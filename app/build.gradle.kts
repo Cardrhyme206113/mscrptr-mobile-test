77973264964f9501c69376a85927532452a7630d
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-08-04 21:04:31 +0300

    app: version native GQA cache build as v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "dev.cardrhyme.muscriptormobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.6.0"
+        versionCode = 11
+        versionName = "0.7.0"
 
         ndk {
             abiFilters += "arm64-v8a"
