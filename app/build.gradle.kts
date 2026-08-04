60dddc844f14a465a2fb917368263e54b7d2d5fa
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-08-04 11:45:26 +0300

    build: bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.cardrhyme.muscriptormobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         ndk {
             abiFilters += "arm64-v8a"
