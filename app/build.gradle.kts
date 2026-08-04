9018860a41f906f527def42f79df30c192707572
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-08-04 13:35:12 +0300

    build: version NPU backend release as 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.cardrhyme.muscriptormobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.3.0"
 
         ndk {
             abiFilters += "arm64-v8a"
